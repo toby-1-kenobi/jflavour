3741ea22da4ec56722bad4ba534b6e4c73f2112d
Toby Anderson <toby.anderson@gmail.com>
2014-09-16 12:02:38 +0530

    Added an installer class in the editor module so I can run code on module load. This will enable me to set up a listener to recieve events from the button(s) passed to the viewer module.

---
+++ JFlavourItemEditor/src/org/sil/jflavouritemeditor/Installer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sil.jflavouritemeditor;
+
+import org.openide.modules.ModuleInstall;
+
+public class Installer extends ModuleInstall
+{
+
+    @Override
+    public void restored()
+    {
+        // TODO
+    }
+
+}
