a5281a59621c82ffdaaad033ed6bd81e3a0aba94
Toby Anderson <toby.anderson@gmail.com>
2014-09-25 22:58:37 +0530

    Finished the category node factory.

---
+++ JFlavourNodeProjectManager/src/org/sil/jflavournodeprojectmanager/CategoryChildFactory.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.sil.jflavournodeprojectmanager;
+
+import java.io.IOException;
+import java.util.List;
+import org.openide.nodes.ChildFactory;
+import org.openide.nodes.Node;
+import org.sil.jflavourapi.JFlavourProjectBean;
+
+/**
+ * A factory to create project nodes.
+ * It needs to read the project data from an XML file
+ * @author toby
+ */
+public class CategoryChildFactory extends ChildFactory<String>
+{
+    
+    JFlavourProjectBean project;
+    
+    public CategoryChildFactory(JFlavourProjectBean project) throws IOException
+    {
+        super();
+        this.project = project;
+    }
+
+    @Override
+    protected boolean createKeys(List<String> list)
+    {
+        list.addAll(project.getCategories());
+        return true;
+    }
+    
+    @Override
+    protected Node createNodeForKey(String key) {
+        return new CategoryNode(key);
+    }
+    
+}
