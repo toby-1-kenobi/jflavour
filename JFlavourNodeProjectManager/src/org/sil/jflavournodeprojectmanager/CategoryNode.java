c0dfbff5a0317151b2372eab46a8b410a923fd84
Toby Anderson <toby.anderson@gmail.com>
2014-09-25 22:24:26 +0530

    Added a node for categories

---
+++ JFlavourNodeProjectManager/src/org/sil/jflavournodeprojectmanager/CategoryNode.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.sil.jflavournodeprojectmanager;
+
+import org.openide.nodes.AbstractNode;
+import org.openide.nodes.Children;
+import org.openide.util.lookup.Lookups;
+
+/**
+ *
+ * @author toby
+ */
+public class CategoryNode extends AbstractNode
+{
+     
+    public CategoryNode(String category) {
+        super (Children.create(new ItemChildFactory(category), true), Lookups.singleton(obj));
+        setDisplayName (category);
+    }
+}
