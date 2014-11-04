f9e2cc82a8a39abdf51fc45e5a35c95761ed1f6f
Toby Anderson <toby.anderson@gmail.com>
2014-11-04 15:24:47 +0530

    Added a super class for the image and audio file of the item. This class lets you import the media files into the project's directory structure. Added the Apache commons-io jar to let me easily work with base name and extension for file names.

---
+++ JFlavourAPI/src/org/sil/jflavourapi/ItemMedia.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.sil.jflavourapi;
+
+import java.io.IOException;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+/**
+ *
+ * @author toby
+ */
+public abstract class ItemMedia
+{
+    
+    protected Path mediaFile;
+
+    public ItemMedia(Path mediaFile)
+    {
+        this.mediaFile = mediaFile;
+    }
+    
+    /**
+     * Copy the media to the application's directory structure
+     * @param targetDir the directory to move the media file to
+     */
+    public void importMedia(Path targetDir) throws IOException
+    {
+        Path mediaFileName = mediaFile.getFileName();
+        Path targetPath = JFlavourPathManager.getAvailablePath(targetDir, mediaFileName);
+        
+        //overwrite existing file, if exists
+        CopyOption[] options = new CopyOption[]{
+          StandardCopyOption.REPLACE_EXISTING,
+          StandardCopyOption.COPY_ATTRIBUTES
+        }; 
+        Files.copy(mediaFile, targetPath, options);
+        mediaFile = targetPath;
+    }
+    
+    /**
+     * Load the media into memory so it is ready for presenting. If it is already loaded do nothing.
+     * @throws java.io.IOException
+     */
+    public abstract void load() throws IOException;
+    
+    /**
+     * unload the media from memory. If it is not loaded do nothing.
+     */
+    public abstract void unload();
+    
+}
--- JFlavourAPI/src/org/sil/jflavourapi/JFlavourPathManager.java
+++ JFlavourAPI/src/org/sil/jflavourapi/JFlavourPathManager.java
@@ -6,6 +6,7 @@ package org.sil.jflavourapi;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import org.apache.commons.io.FilenameUtils;
 
 /**
  * Manages paths for storage and retreival of JFlavour application data
@@ -31,5 +32,31 @@ public class JFlavourPathManager
             directory = Paths.get(System.getProperty("user.home"), "/.JFlavour");
         }
         return directory;
+    }
+    
+    /**
+     * Return a Path to a file that does not exists given a directory and target filename.
+     * If the filename is not already used in a given directory then that filename is returned, otherwise a similar
+     * filename should be found that is not used in that directory
+     * @param directory The directory in which the availabl filename must be found
+     * @param targetFileName the target filename to base the search on
+     * @return the available Path
+     */
+    public static Path getAvailablePath(Path directory, Path targetFileName)
+    {
+        if (directory.resolve(targetFileName).toFile().exists())
+        {
+            int i = 1;
+            String baseName = FilenameUtils.getBaseName(targetFileName.toString());
+            String extension = FilenameUtils.getExtension(targetFileName.toString());
+            while (directory.resolve(baseName + i + '.' + extension).toFile().exists())
+            {
+                ++i;
+            }
+            //TODO: We've just found a path that is available, bt from this point on it may become unavaible
+            // at any time until it is used. So really we should use some sort of lock on the filesystem for this filename.
+            targetFileName = Paths.get(baseName + i + '.' + extension);
+        }
+        return directory.resolve(targetFileName);
     }
 }
